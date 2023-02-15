907313a4c3ffbfbec3c0620aa5ad2862cdf4ce0d
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-02-15 16:12:57 +0900

    settings: frontend start setting

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended", // plugin과 eslint-config-prettier 설정을 한번에 합니다.
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
