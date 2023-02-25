b8febafb1f62dc5abf28f3160433d50ae58a4702
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-02-26 02:34:15 +0900

    feat: create large button component

--- frontend/src/pages/quizitem.tsx
+++ frontend/src/pages/quizitem.tsx
@@ -1,3 +1,10 @@
+import LargeButton from "@/components/atoms/buttons/LargeButton";
+
 export default function QuizItem() {
-  return <div>개별 문제 조회</div>;
+  return (
+    <div>
+      <div>개별 문제 조회</div>
+      <LargeButton />
+    </div>
+  );
 }
