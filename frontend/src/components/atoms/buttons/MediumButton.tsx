43eeae7fadeb936bc9f7736f4aed8fd1e2eb3c41
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-02-27 18:31:05 +0900

    feat: create medium button component

--- frontend/src/components/atoms/buttons/MediumButton.tsx
+++ frontend/src/components/atoms/buttons/MediumButton.tsx
@@ -1,3 +1,23 @@
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  font-family: "Press Start 2P";
+  width: 50%;
+  height: 50px;
+  background-color: #26bba0;
+  font-size: 2em;
+  border: none;
+  color: white;
+  &:hover {
+    cursor: pointer;
+    background-color: #25b097;
+  }
+`;
+
 export default function MediumButton() {
-  return <div>중간 버튼</div>;
+  return (
+    <div>
+      <Button>START</Button>
+    </div>
+  );
 }
