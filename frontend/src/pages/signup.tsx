9754b358b6549a3ded1ecd893d0c2f6858cfed14
drsuneamer <sunyeong0412@gmail.com>
2023-03-01 03:53:30 +0900

    feat: add header in pages

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,3 +1,15 @@
+import styled from "@emotion/styled";
+import SignUpBody from "@/components/template/auth/SignUpBody";
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 export default function SignUp() {
-  return <div>SignUP</div>;
+  return (
+    <Div>
+      <SignUpBody />
+    </Div>
+  );
 }
