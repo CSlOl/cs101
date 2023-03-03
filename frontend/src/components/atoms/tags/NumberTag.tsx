4f18a3601fdd0494afcaccfc520f6b963efe26d9
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-03 14:45:50 +0900

    feat: add number tag

--- frontend/src/components/atoms/tags/NumberTag.tsx
+++ frontend/src/components/atoms/tags/NumberTag.tsx
@@ -1,3 +1,21 @@
+import styled from "@emotion/styled";
+
+const NumTag = styled.button`
+  width: 50px;
+  height: 20px;
+  background-color: #00000;
+  font-size: 1em;
+  border-radius: 20px;
+  border: none;
+  color: white;
+\
+`;
+
+// 문제 리스트 사이드바에서 해당 카테고리에 포함된 문제 갯수를 보여주는 태그
 export default function NumberTag() {
-  return <div>[30]</div>;
+  return (
+    <div>
+      <NumTag>30</NumTag>
+    </div>
+  );
 }
