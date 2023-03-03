b0372ea531ad2ff7854a1894df34b7e99a966f7a
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-03 14:45:39 +0900

    feat: add large tag

--- frontend/src/components/atoms/tags/LargeTag.tsx
+++ frontend/src/components/atoms/tags/LargeTag.tsx
@@ -1,3 +1,20 @@
+import styled from "@emotion/styled";
+
+const Tag = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: #26bba0;
+  font-size: 1.8em;
+  border-radius: 10px;
+  border: none;
+  color: white;
+`;
+
+// 개별 문제 내 문제 설명해주는 태그 (이미 푼 문제 / 자료구조 / 객관식 등)
 export default function LargeTag() {
-  return <div>큰 태그</div>;
+  return (
+    <div>
+      <Tag>😎이미 푼 문제</Tag>
+    </div>
+  );
 }
