482a24524dbf9ef8fa7bff414759995e6973cb7c
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-10 01:02:44 +0900

    feat: add conditional for small tags

--- frontend/src/components/atoms/tags/SmallTag.tsx
+++ frontend/src/components/atoms/tags/SmallTag.tsx
@@ -1,20 +1,21 @@
 import styled from "@emotion/styled";
 
 const Tag = styled.button`
-  width: 110px;
+  width: 100px;
   height: 30px;
   background-color: #26bba0;
   font-size: 1.1em;
   border-radius: 10px;
   border: none;
   color: white;
+  margin-right: 5px;
 `;
 
 // 문제 리스트에 함께 표시되는 작은 태그
-export default function SmallTag() {
+export default function SmallTag(props: any) {
   return (
     <div>
-      <Tag>자료구조</Tag>
+      {props.type ? <Tag>{props.type}</Tag> : <Tag>{props.category}</Tag>}
     </div>
   );
 }
