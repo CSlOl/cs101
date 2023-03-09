edab488bb2f7c4fcfbf132166834265cb993fbd4
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-10 00:14:13 +0900

    feat: add personal quiz card outline

--- frontend/src/components/molecules/PersonalQuizCard.tsx
+++ frontend/src/components/molecules/PersonalQuizCard.tsx
@@ -1,3 +1,49 @@
-export default function PersonalQuizCard() {
-  return <div>멀티 프로세스와 멀티 스레드의 차이, 내가 푼 문제(맞은문제, 틀린문제, 즐겨찾기)</div>;
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const Card = styled.div`
+  margin: 8px 8px 0 0;
+  width: 100%;
+  height: 90%;
+
+  .cardHeader {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 7px 7px 0 0;
+    font-size: 0.9em;
+    width: 45%;
+    height: 25px;
+  }
+
+  .cardList {
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 7px 0 7px 7px;
+    height: 85%;
+  }
+`;
+
+// 멀티 프로세스와 멀티 스레드의 차이, 내가 푼 문제(맞은문제, 틀린문제, 즐겨찾기)
+
+export default function PersonalQuizCard(props: any) {
+  const [quizType, setQuizType] = useState<string>("맞은문제");
+
+  return (
+    <Card>
+      <div className="cardHeader">
+        <div className="buffer" />
+        <div className="title">
+          {props.type} ({props.count})
+        </div>
+      </div>
+      <div className="cardList"></div>
+    </Card>
+  );
 }
--- frontend/src/components/organisms/mypage/PersonalQuizForm.tsx
+++ frontend/src/components/organisms/mypage/PersonalQuizForm.tsx
@@ -1,15 +1,47 @@
+import PersonalQuizCard from "@/components/molecules/PersonalQuizCard";
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 const Container = styled.div`
   height: 49%;
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.2);
+  padding: 15px;
+  font-family: "DungGeunMo";
+
+  .personalQuizHeader {
+    font-size: 1.5em;
+    color: #26bba0;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  a {
+    color: white;
+    font-size: 0.5em;
+  }
+
+  .cards {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+  }
 `;
 
 export default function PersonalQuizForm() {
+  const [quizCount, setQuizCount] = useState<number>(20);
+
   return (
     <Container>
-      <div>내가 푼 문제</div>
+      <div className="personalQuizHeader">
+        <p>내가 푼 문제</p>
+        <a href="">-&gt; 전체보기</a>
+      </div>
+      <div className="cards">
+        <PersonalQuizCard type="맞은문제" count={quizCount} />
+        <PersonalQuizCard type="틀린문제" count={quizCount} />
+        <PersonalQuizCard type="즐겨찾기" count={quizCount} />
+      </div>
     </Container>
   );
 }
