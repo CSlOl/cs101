c7fe07d411b4628bd00205c019e2bb205492595f
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-15 00:16:41 +0900

    feat: relocate user info div to molecules

--- frontend/src/components/organisms/mypage/UserInfoForm.tsx
+++ frontend/src/components/organisms/mypage/UserInfoForm.tsx
@@ -7,11 +7,17 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
+  align-items: center;
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   color: white;
+
+  @media (max-width: 768px) {
+    flex-direction: row;
+    height: 50%;
+  }
 `;
 
 const UserInfoDiv = styled.div`
@@ -25,13 +31,37 @@ const UserInfoDiv = styled.div`
   }
 `;
 
+const ProfileInfoDiv = styled.div`
+  text-align: center;
+  color: white;
+  margin-top: 25px;
+  @media (max-width: 768px) {
+    margin-top: 0px;
+    margin-bottom: 2.5em;
+  }
+
+  .nickName {
+    font-size: 30px;
+  }
+
+  .email {
+    font-size: 14px;
+  }
+`;
+
 export default function UserInfoForm() {
   const [probNum, setProbNum] = useState<number>(1234);
+  const [nickName, setNickName] = useState<string>("zl존규민");
+  const [email, setEmail] = useState<string>("gyumin.q.lee@gmail.com");
 
   return (
     <Container>
       <ProfileImage />
       <UserInfoDiv>
+        <ProfileInfoDiv>
+          <div className="nickName">{nickName}</div>
+          <div className="email">{email}</div>
+        </ProfileInfoDiv>
         <Link href="/editprofile">
           <div className="toProfileEdit">회원정보수정</div>
         </Link>
