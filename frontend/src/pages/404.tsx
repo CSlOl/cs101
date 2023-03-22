2a103d8b8ba50adeba8b168570c551b6fa3f7406
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-03-23 02:07:14 +0900

    design: add ball movement to 404 page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,6 +1,65 @@
 import styled from "@emotion/styled";
+import Image from "next/image";
+import { keyframes } from "@emotion/react";
+
+const move = keyframes`
+  // 360도 회전시키기
+  0%, 20%, 40%, 60%, 80% {
+    transform: rotate(0deg)
+  }
+  10%, 30%, 50%, 70%, 90% {
+    transform: rotate(360deg)
+  }
+
+  // 배경에서 움직이기
+  from {
+    margin-left: -100%;
+    margin-top: -50%;
+  }
+  10% {
+    margin-left: 100%;
+    margin-top: 0%;
+  }
+  20%{
+    margin-left: 0%;
+    margin-top: 44%;
+  }
+  30%{
+    margin-left: 50%;
+    margin-top: -50%;
+  }
+  40%{
+    margin-left: -50%;
+    margin-top: 44%;
+  }
+  50% {
+    margin-left: 50%;
+    margin-top: -50%;
+  }
+  60%{
+    margin-left: -100%;
+    margin-top: 0%;
+  }
+  70%{
+    margin-left: 100%;
+    margin-top: 44%;
+  }
+  80% {
+    margin-left: -100%;
+    margin-top: -50%;  
+  }
+  90%{
+    margin-left: 100%;
+    margin-top: 44%;
+  }
+  to {
+    margin-left: -100%;
+    margin-top: -50%;
+  }
+`;
 
 const Container = styled.div`
+  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -42,16 +101,42 @@ const Container = styled.div`
       color: #000000;
     }
   }
+
+  .green {
+    position: absolute;
+    animation: ${move} linear 50s infinite;
+  }
+
+  .purple {
+    position: absolute;
+    animation: ${move} linear 30s infinite;
+  }
 `;
 
 export default function NotFound() {
   return (
-    <Container>
-      <div className="box">
-        <h1>404 NOT FOUND</h1>
-        <p>An error has occurred, to continue:</p>
-        <a href="/">&gt; 홈으로 돌아가기</a>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <div className="box">
+          <h1>404 NOT FOUND</h1>
+          <p>An error has occurred, to continue:</p>
+          <a href="/">&gt; 홈으로 돌아가기</a>
+        </div>
+        <Image
+          className="purple"
+          src="/circle_purple.png"
+          alt="circle_purple"
+          width={100}
+          height={95}
+        />
+        <Image
+          className="green"
+          src="/circle_basic.png"
+          alt="circle_basic"
+          width={100}
+          height={95}
+        />
+      </Container>
+    </>
   );
 }
