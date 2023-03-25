de03116bf03d9a368aca919d65365bec2d932952
drsuneamer <sunyeong0412@gmail.com>
2023-03-26 01:06:25 +0900

    feat: read quizitem (multiple) layout

--- frontend/src/components/organisms/readquiz/ReadMultiple.tsx
+++ frontend/src/components/organisms/readquiz/ReadMultiple.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import QuestionBox from "@/components/atoms/boxes/QuestionBox";
+import SelectedOption from "@/components/atoms/options/SelectedOption";
+
 export default function ReadMultiple() {
-  return <div>객관식 조회</div>;
+  const [solved, setSolved] = useState(true);
+  const [question, setQuestion] = useState(
+    "다음 중 스택(stack)에 관한 설명 중 옳지 않은 것은?"
+  );
+
+  const options = [
+    "First-In, Last-Out의 구조를 가지고 있다.",
+    "큐(Queue)와 동일한 구조를 가지고 있다.",
+    "삽입과 삭제가 일어나는 곳이 같다.",
+    "가장 먼저 저장되는 데이터는 스택의 아래 쪽부터 쌓인다.",
+  ];
+
+  return (
+    <div>
+      <QuestionBox question={question} />
+      {options.map((option, index) => (
+        <SelectedOption num={index} text={option}></SelectedOption>
+      ))}
+    </div>
+  );
 }
