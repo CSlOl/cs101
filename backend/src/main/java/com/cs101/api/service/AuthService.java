ebb43cb9b4f30a793e028a3bf160dea1eb875add
kompany11@hanmail.net <kompany11@hanmail.net>
2023-03-26 18:04:30 +0900

    fix: add transactional annotation to auth service

--- backend/src/main/java/com/cs101/api/service/AuthService.java
+++ backend/src/main/java/com/cs101/api/service/AuthService.java
@@ -8,11 +8,13 @@ import com.cs101.entity.UserStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AuthService {
 
