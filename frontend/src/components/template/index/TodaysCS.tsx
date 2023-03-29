8aeb67ba342a41d2a5f9dd246ac1399aa504631b
drsuneamer <sunyeong0412@gmail.com>
2023-03-29 21:23:11 +0900

    feat: index - main page 2 layout

--- frontend/src/components/template/index/TodaysCS.tsx
+++ frontend/src/components/template/index/TodaysCS.tsx
@@ -1,14 +1,17 @@
 import styled from "@emotion/styled";
+
 const Title = styled.div`
   position: absolute;
   top: 38%;
   font-size: 1.25em;
+  cursor: default;
 `;
 
 const Content = styled.div`
   position: absolute;
   top: 48%;
   font-size: 2em;
+  cursor: default;
 `;
 export default function TodaysCS() {
   return (
