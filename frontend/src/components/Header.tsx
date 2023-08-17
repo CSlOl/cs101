30a0d715795d7f735faf2b4b7eb75d28a4d8ca53
drsuneamer <sunyeong0412@gmail.com>
2023-08-17 22:17:24 +0900

    feat: add mypage/logout in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,10 @@
+import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
 import styled from "@emotion/styled";
+import { useState, useEffect } from "react";
+import swal from "sweetalert";
+import { useRouter } from "next/router";
 
 const Div = styled.div`
   width: 100%;
@@ -39,29 +43,62 @@ const Text = styled.div`
 `;
 
 export default function Header() {
+  const router = useRouter();
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken") !== null) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }, []);
+
+  function logout() {
+    axios.get(`${baseURL}/api/auth/logout`).then((res) => {
+      console.log(res);
+      if (res.data.statusCode === 200) {
+        localStorage.clear();
+        setLogin(false);
+        router.push("/");
+      } else if (res.data.statusCode === 500) {
+        swal("로그아웃에 실패하였습니다.", {
+          icon: "error",
+        });
+      }
+    });
+  }
+
   return (
     <Div>
       <Link href="/">
         <Logo>
-          <Image
-            src="/circle_basic.png"
-            alt="circle_basic"
-            width={50}
-            height={48}
-          />
+          <Image src="/circle_basic.png" alt="circle_basic" width={50} height={48} />
           <Title>CS101</Title>
         </Logo>
       </Link>
 
-      <Move>
-        <Link href="/login">
-          <Text>login</Text>
-        </Link>
-        <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
-        <Link href="/signup">
-          <Text>signup</Text>
-        </Link>
-      </Move>
+      {login ? (
+        <Move>
+          <Link href="/mypage">
+            <Text>my page</Text>
+          </Link>
+          <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
+          <Text onClick={logout}>logout</Text>
+        </Move>
+      ) : (
+        <Move>
+          <Link href="/login">
+            <Text>login</Text>
+          </Link>
+          <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
+          <Link href="/signup">
+            <Text>signup</Text>
+          </Link>
+        </Move>
+      )}
     </Div>
   );
 }
