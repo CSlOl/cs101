21d4f05da2bb28836f69afdbf5abc69651b0e9d5
drsuneamer <sunyeong0412@gmail.com>
2023-08-18 15:22:18 +0900

    feat: edit interceptor

--- frontend/src/interceptor.tsx
+++ frontend/src/interceptor.tsx
@@ -1,19 +1,45 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import swal from "sweetalert";
+// import { useRouter } from "next/router";
+
 
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const [accessToken, setToken]: any = useState("");
+// const router = useRouter();
 
-useEffect(() => {
-  setToken(localStorage.getItem("accessToken"));
-}, []);
-
-// 요청 전 처리
-const api = axios.create({
+const api: AxiosInstance = axios.create({
   baseURL,
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-  },
 });
+
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    // localStorage에 accessToken 있는 경우 헤더에 추가
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    } else {
+      swal("다시 로그인해주세요.", {
+        icon: "error",
+      });
+      // router.push("/");
+    }
+
+    return config;
+  }
+)
+
+// 만료 등 설정 확인 후 response 추가할 것
+// api.interceptors.response.use(
+//   (response: AxiosResponse) => {
+//     // Modify response data if needed
+//     return response;
+//   },
+//   (error) => {
+//     // Handle errors globally (e.g., show notifications)
+//     return Promise.reject(error);
+//   }
+// );
+
 
 export default api;
